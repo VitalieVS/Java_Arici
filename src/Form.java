b4748eac357bae99842a11ac70e221c1681c4684
VitalieVSV <dut66AHJCT>
2020-12-13 12:56:17 +0200

    Method of adding a form

---
+++ src/Form.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Form extends JFrame {
+    public Form() {
+        this.setTitle("Hello World");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(new Dimension(400,400));
+        this.setVisible(true);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,6 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("work");
-
-        //frame
-        JFrame frame = new JFrame("Hello"); // create frame / w title
-        frame.setSize(new Dimension(200,200)); // set size
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        //Hide_on close
-        //do nothing
-        //exit on close
-        frame.setResizable(false); // resizible
-        //icon
-        frame.setVisible(true);
-
-        ImageIcon image = new ImageIcon("calendar.png"); //not work, path is correct
-        frame.setIconImage(image.getImage());
-
-         // make visible
+        new Form();
     }
 }
