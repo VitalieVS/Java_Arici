3bc0ab97d7e111afec9779874f36ce05a22f6725
VitalieVSV <dut66AHJCT>
2020-12-13 13:06:36 +0200

    panels

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,13 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("work");
-        new Form();
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.BLACK);
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setSize(750,750);
+        frame.setVisible(true);
     }
 }
