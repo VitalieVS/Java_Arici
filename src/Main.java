0da78f9439ee7d64a0f150c3785892b22ec71491
VitalieVSV <dut66AHJCT>
2020-12-13 13:37:09 +0200

    Arici

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,61 @@
-import javax.swing.*;
-import java.awt.*;
+import org.junit.Test;
 
 public class Main {
-    public static void main(String[] args) {
-        JPanel panel = new JPanel();
-        panel.setBackground(Color.BLACK);
+    public int[][] array = new int[3][4];
 
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setLayout(null);
-        frame.setSize(750,750);
-        frame.setVisible(true);
+    @Test
+    public void arici() {
+        initializeArray();
+        showArray();
+        System.out.println(solve());
+    }
+
+    public void initializeArray() {
+        array[0][0] = 69;
+        array[0][1] = 70;
+        array[0][2] = 80;
+        array[0][3] = 90;
+        array[1][0] = 1;
+        array[1][1] = 2;
+        array[1][2] = 3;
+        array[1][3] = 4;
+        array[2][0] = 100;
+        array[2][1] = 20;
+        array[2][2] = 60;
+        array[2][3] = 40;
+    }
+
+    int solve(){
+      int value = 0;
+      for (int i = 0; i < 4 ; i++) {
+          value += getMax();
+      }
+      return value;
+    }
+
+    int getMax() {
+        int coordI = 0;
+        int coordJ = 0;
+        int init = array[0][0];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (init < array[i][j]){
+                    init = array[i][j];
+                    coordI = i;
+                    coordJ = j;
+                }
+            }
+        }
+        array[coordI][coordJ] = 0;
+        return init;
+    }
+
+    public void showArray() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
