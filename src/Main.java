3b0d786551eb838360c8d2df90d43a3e80a99313
VitalieVSV <dut66AHJCT>
2020-12-13 12:42:07 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("work");
+
+        //frame
+        JFrame frame = new JFrame(); // create
+        frame.setSize(new Dimension(200,200)); // set size
+        frame.setVisible(true); // make visible
+    }
+}
